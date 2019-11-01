24217123932b63aff163aacd223242352d58cee5
suraj-balvanshi <surajbalvanshi1@gmail.com>
2019-11-01 10:06:30 +0530

    loading page

---
+++ loading.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="css/loading.css">
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div class="arc">
+    </div>
+    <h1><span>LOADING</span></h1>
+  </body>
+</html>
